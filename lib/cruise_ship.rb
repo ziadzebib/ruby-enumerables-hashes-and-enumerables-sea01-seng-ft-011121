1f7b26c6a63f1595957a2b67535c6aacf98cdc68
Ziad Zebib <zebibziad@gmail.com>
2021-01-02 03:14:39 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,12 +7,12 @@
 # }
 
 def select_winner(passengers)
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
 end
  
-winner
+  winner
 end
